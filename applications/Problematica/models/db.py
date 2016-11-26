1a1d5ed918a90772f53927e93d8e1a783c837b5a
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 23:40:29 -0800

    trying to fix issue regarding solutions table, try 3

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -157,8 +157,8 @@ auth.settings.create_user_groups = "user"
 
 if os.environ.get("IS_TEST",None):
     # Create a test database that's laid out just like the "real" database
-    #import copy
+    import copy
     test_db = db  # Name and location of the test DB file
-    '''for tablename in db.tables:  # Copy tables!
+    for tablename in db.tables:  # Copy tables!
         table_copy = [copy.copy(f) for f in db[tablename]]
-        test_db.define_table(tablename, *table_copy)'''
+        test_db.define_table(tablename, *table_copy)
