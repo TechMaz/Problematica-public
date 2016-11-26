cbb4993824c8bf84932707f740b3c90c051bc8a3
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 23:43:19 -0800

    trying to fix issue regarding solutions table, try 4.1

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -161,4 +161,4 @@ if os.environ.get("IS_TEST",None):
     test_db = db  # Name and location of the test DB file
     '''for tablename in db.tables:  # Copy tables!
         table_copy = [copy.copy(f) for f in db[tablename]]
-        test_db.define_table(tablename, *table_copy)''
+        test_db.define_table(tablename, *table_copy)'''
