17c13a6d3544bea77bcd5468785c69b059a98171
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 17:37:21 -0800

    seeing if this fixes build 4.

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -36,7 +36,7 @@ if not request.env.web2py_runtime_gae:
             migrate_enabled=myconf.get('db.migrate'),
             check_reserved=['all'])
     else:
-        db = DAL('sqlite://testing.sqlite',migrate_enabled='false',check_reserved=['all'])
+        db = DAL('sqlite://testing.sqlite')
 
 else:
     # ---------------------------------------------------------------------
