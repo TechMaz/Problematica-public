232166b0d70ad89fc4ceb8b2ee3d4cc426354a3f
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 17:27:54 -0800

    seeing if this fixes build.

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -36,7 +36,7 @@ if not request.env.web2py_runtime_gae:
             migrate_enabled=myconf.get('db.migrate'),
             check_reserved=['all'])
     else:
-        db = DAL('sqlite://testing.sqlite')
+        db = DAL('sqlite://testing.sqlite',migrate_enabled='false')
 
 else:
     # ---------------------------------------------------------------------
@@ -153,8 +153,8 @@ auth.settings.create_user_groups = "user"
 
 
 # Create a test database that's laid out just like the "real" database
-import copy
-test_db = DAL('sqlite://testing.sqlite')  # Name and location of the test DB file
-for tablename in db.tables:  # Copy tables!
+#import copy
+test_db = DAL('sqlite://testing.sqlite',migrate_enabled='false')  # Name and location of the test DB file
+'''for tablename in db.tables:  # Copy tables!
     table_copy = [copy.copy(f) for f in db[tablename]]
-    test_db.define_table(tablename, *table_copy)
+    test_db.define_table(tablename, *table_copy)'''
