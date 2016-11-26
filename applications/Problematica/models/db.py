58512e80c6617c2044a9bf0bb7367f3136f6545b
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 23:06:37 -0800

    trying to add additional tables to test db, try2

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -151,3 +151,65 @@ auth.settings.reset_password_requires_verification = True
 
 #default new accounts to user group
 auth.settings.create_user_groups = "user"
+
+if os.environ.get("IS_TEST",None):
+    # Create a test database that's laid out just like the "real" database
+    #import copy
+    test_db = db  # Name and location of the test DB file
+    import datetime
+    test_db.define_table('problems_by_user',
+                    Field('user_id', type='integer'),
+                    Field('problem_id', type='integer'))
+
+    test_db.define_table('images',
+        Field('picture', 'upload', uploadfield='picture_file'),
+        Field('picture_file', 'blob'),
+    	Field('owner_id', type='integer')
+    )
+
+    test_db.define_table('problems',
+                    Field('id'),
+                    Field('problem_title', type='string'),
+                    Field('time_access', type='datetime', default=datetime.datetime.utcnow()),
+                    Field('problem_text', type='text'),
+                    Field('problem_about', type='text'),
+                    Field('problem_implications', type='text'),
+                    Field('problem_updates', type='text'),
+                    Field('num_donors', type='integer'),
+                    Field('status', requires=IS_IN_SET(['open','closed'])),
+    #                Field('initial_bounty', type='double', default=0),
+                    Field('initial_bounty', type='double', default=0),
+                    Field('current_bounty', type='double', default=0),
+                    Field('date_posted', type='datetime', default=datetime.datetime.utcnow()),
+                    Field('subject', requires=IS_IN_DB(test_db,'topics.topic_name')),
+                    Field('institution', type='integer', default=auth.user_id)
+    )
+
+    test_db.define_table('donations',
+                    Field('id'),
+                    Field('donater_id', type='integer', default=auth.user_id),
+                    Field('problem_id', type='integer', default=0),
+                    Field('amount', type='integer', default='0'),
+                    Field('donor_message', type='string'),
+                    Field('date_made', type='datetime', default=datetime.datetime.utcnow())
+    )
+
+    test_db.define_table('solutions',
+                    Field('id'),
+                    Field('attempter_id', type='integer', default='0', requires=IS_NOT_EMPTY()),
+                    Field('problem_id', type='integer', default='0', requires=IS_NOT_EMPTY()),
+                    Field('link_to_solution', type='string', default='', requires=IS_NOT_EMPTY()),
+                    Field('solution_pdf', type='upload', uploadfield='pdf_file'),
+                    Field('pdf_file', 'blob'),
+                    Field('status', requires=IS_IN_SET(['right','wrong','pending', 'too late']), default='pending'),
+                    Field('submitter_comment'),
+                    Field('date_submitted', type='datetime', default=datetime.datetime.utcnow())
+    )
+
+    test_db.define_table('topics',
+        Field('topic_name', type='string'),
+    	Field('topic_description', type='text')
+    )
+    '''for tablename in db.tables:  # Copy tables!
+        table_copy = [copy.copy(f) for f in db[tablename]]
+        test_db.define_table(tablename, *table_copy)'''
