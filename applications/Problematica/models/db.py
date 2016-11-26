f84a718219d0c77eee267533464fab3799d798b9
Steven Mazliach <smazliac@ucsc.edu>
2016-11-25 17:34:45 -0800

    seeing if this fixes build 3.

--- applications/Problematica/models/db.py
+++ applications/Problematica/models/db.py
@@ -154,8 +154,8 @@ auth.settings.create_user_groups = "user"
 
 
 # Create a test database that's laid out just like the "real" database
-#import copy
-test_db = DAL('sqlite://testing.sqlite',migrate_enabled='false')  # Name and location of the test DB file
-'''for tablename in db.tables:  # Copy tables!
+import copy
+test_db = DAL('sqlite://testing.sqlite')  # Name and location of the test DB file
+for tablename in db.tables:  # Copy tables!
     table_copy = [copy.copy(f) for f in db[tablename]]
-    test_db.define_table(tablename, *table_copy)'''
+    test_db.define_table(tablename, *table_copy)
